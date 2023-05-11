904fa9b9f5af078bfd1c634eb3700ba5bcd45c5e
Erembe <uuerembe@gmail.com>
2023-05-11 19:55:25 +0100

    Created a function

--- exercise2.py
+++ exercise2.py
@@ -3,3 +3,12 @@ print(message)
 
 new_message = "This is another message."
 print(new_message)
+
+another_one = "Message again o!"
+print(another_one)
+
+def greet(name):
+    return f"Hello, {name}"
+
+greeting = greet("Erembe")
+print(greeting)
